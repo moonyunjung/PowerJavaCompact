13d714407a6173ac412149b1ea4eb71e2f9af8f1
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-07-31 21:40:31 +0900

    Add files via upload

---
+++ DogTest/Animal.java
@@ -0,0 +1,5 @@
+public class Animal {
+	void eat() {
+		System.out.println("먹고 있음...");
+	}
+}
---
+++ DogTest/DogTest.java
@@ -0,0 +1,10 @@
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.bark();
+		d.eat();
+	}
+
+}
